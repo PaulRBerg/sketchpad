5ead0ce930a382d59cefcc87ab3bd141f2113f9a
Paul Berg <hello@paulrberg.com>
2018-05-29 18:29:18 +0100

    Implemented killme feature in node spawn

--- node/child_process/spawn.js
+++ node/child_process/spawn.js
@@ -1,13 +1,27 @@
+const net = require('net');
 const spawn = require('child_process').spawn;
 
-let bears = 1;
+let bears = 0;
+
+bears += 1;
 
 if (process.argv[2] === 'child') {
-	console.log('I\'m inside the child');
+	const pipe = new net.Socket({ fd: 3}); // third file descriptor according to stdio below
+	pipe.write('killme');
+
+	// console.log('child', bears);
 } else {
 	const child = spawn(process.execPath, [__filename, 'child'], {
-		stdio: 'inherit'
+		stdio: [null, null, null, 'pipe']
 	});
+	child.stdio[3].on('data', function (data) {
+		if (data.toString() === 'killme') {
+			console.log('RIP');
+			child.kill();
+		}
+	});
+
+	// console.log('parent', bears);
 
 	// child.stdout.pipe(process.stdout);
 
